781d27de29cfe78666cfdb57e826444859c4809b
Zhijian Zeng <wintera.zeng@gmail.com>
2012-01-30 22:46:53 +0800

    Re-sharp the package design

---
+++ src/com/kanezeng/WebSpider/Items/DiscountItems.java
@@ -0,0 +1,44 @@
+package com.kanezeng.WebSpider.Items;
+
+import com.kanezeng.utils.DateTime;
+
+
+/**
+ * @author Zhijian Zeng
+ * Email: me@kanezeng.com
+ * This is a parent object for all items.
+ */
+public class DiscountItems {
+	public String name = "";
+	public String fromURL = "";
+	public String targetLink = "";
+	public String currentPrice = "";
+	public String originalPrice = "";
+	public String discountRate = "";
+	public String updatedDate = "";
+	
+	public DiscountItems(String name,String fromURL, String targetLink, String currentPrice, String originalPrice, String discountRate) {
+		this.name = name;
+		this.currentPrice = currentPrice;
+		this.fromURL = fromURL;
+		this.targetLink = targetLink;
+		this.originalPrice = originalPrice;
+		this.discountRate = discountRate;
+		this.updateDate();
+	}
+	
+	/**
+	 * This method will update the date & time for current object.
+	 */
+	private void updateDate() {
+		this.updatedDate = DateTime.getSystemTime();
+	}
+	
+	/**
+	 * This method will extract all possible information from the input file.
+	 * @param inputFile The file you would like to extract information from.
+	 */
+	public void extractInfoFromFile(String inputFile) {
+		this.updateDate();
+	}
+}
